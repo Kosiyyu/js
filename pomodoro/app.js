39ab3fdbadabbff892a50c2c52bc7460e65c9da4
Karol Koś <83028094+Kosiyyu@users.noreply.github.com>
2022-04-15 17:46:23 +0200

    adding date and time format to "Timer" class

--- pomodoro/app.js
+++ pomodoro/app.js
@@ -1,9 +1,16 @@
 const id = ["pomodoro","break-5","break-15"] ;
 
+const myTime = new Intl.DateTimeFormat("default",{
+    minute: "numeric",
+    second: "numeric",
+    hour12: false
+})
+
 const Timer = class {
     constructor(name, seconds) {
         this.name = name;
         this.seconds = seconds;
+        this.date = new Date(0, 0, 0, 0, 0, 0, seconds);
         this.break = false;
     }
 
@@ -16,21 +23,29 @@ const Timer = class {
         this.togglePause.bind(this);
         function test(){
             if(this.seconds <= 0){
-                console.log(this.seconds);
+                //console.log(this.seconds);
+                this.date.setSeconds(this.seconds);
+                console.log(myTime.format(this.date));
                 clearInterval(counter);
             }
             else if(this.break){
                 clearInterval(counter);
             }
             else {
-                console.log(this.seconds);
+                //console.log(this.seconds);
+                this.date.setSeconds(this.seconds);
+                console.log(myTime.format(this.date));
             this.seconds--;
             }
         }
     }
   };
 
-const pomodoro = new Timer("pomodoro",3);
+// let date = new Date(0, 0, 0, 0, 0, 0, 0);
+// date.setSeconds(1500);
+// console.log(dateTimeFormat.format(date));
+
+const pomodoro = new Timer("pomodoro",25);
 pomodoro.startTimer();
 
-
+console.log(pomodoro.name);
