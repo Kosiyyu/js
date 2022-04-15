a4056d36a8152a52e4a433d29e47bf9dd488213d
Karol Koś <83028094+Kosiyyu@users.noreply.github.com>
2022-04-15 20:24:52 +0200

    updating elements with "Timer" class data and setting new id's + fixing timer display bug

--- pomodoro/app.js
+++ pomodoro/app.js
@@ -7,11 +7,11 @@ const myTime = new Intl.DateTimeFormat("default",{
 })
 
 const Timer = class {
-    constructor(name, seconds) {
-        this.name = name;
+    constructor(id, seconds) {
+        this.id = id;
         this.seconds = seconds;
         this.date = new Date(0, 0, 0, 0, 0, 0, seconds);
-        this.break = false;
+        this.break = true;
     }
 
     togglePause(){
@@ -23,29 +23,52 @@ const Timer = class {
         this.togglePause.bind(this);
         function test(){
             if(this.seconds <= 0){
-                //console.log(this.seconds);
+                this.date.setMinutes(0);
                 this.date.setSeconds(this.seconds);
-                console.log(myTime.format(this.date));
+                this.updateTimer(myTime.format(this.date));
                 clearInterval(counter);
             }
             else if(this.break){
                 clearInterval(counter);
             }
             else {
-                //console.log(this.seconds);
+                this.date.setMinutes(0);
                 this.date.setSeconds(this.seconds);
-                console.log(myTime.format(this.date));
+                this.updateTimer(myTime.format(this.date));
             this.seconds--;
             }
         }
     }
+
+    //update html
+    updateTimer(seconds){
+        const element = document.getElementById("timer-" + this.id);
+        element.textContent = seconds;
+    }
+
+    //update html   
+    updateButton(){
+        const element = document.getElementById("button-" + this.id);
+        function listen(){
+            this.togglePause();
+            if(this.break){
+                element.textContent = "start";
+            }
+            else {
+                if(!this.break){
+                    this.startTimer(this.seconds);
+                    element.textContent = "stop";
+                }
+            }
+        }
+        element.addEventListener("click", listen.bind(this))
+    }
+
   };
 
-// let date = new Date(0, 0, 0, 0, 0, 0, 0);
-// date.setSeconds(1500);
-// console.log(dateTimeFormat.format(date));
-
-const pomodoro = new Timer("pomodoro",25);
-pomodoro.startTimer();
-
-console.log(pomodoro.name);
+const pomodoro = new Timer("pomodoro",1499);
+const break5 = new Timer("break5",299)
+const break15 = new Timer("break15",899)
+pomodoro.updateButton();
+break5.updateButton();
+break15.updateButton();
